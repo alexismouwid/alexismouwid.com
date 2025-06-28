80a056d07f360d2fb3f656455747480059ca85ef
alexismouwid <alexismouwid@gmail.com>
2025-06-28 10:38:42 -0500

    agg components scrollprogress

--- src/utils/scrollTrigger.js
+++ src/utils/scrollTrigger.js
@@ -18,54 +18,54 @@ export default function useScrollTrigger() {
         trigger: "#content-container",
         start: "top top",
         end: "bottom bottom",
-        scrub: 1,
+        scrub: 0.5,
         pin: true,
         pinSpacing: false,
         snap: {
           snapTo: 1 / 18, // 10 secciones (9 saltos)
-          duration: { min: 0.2, max: 1 },
+          duration:  9999,
           delay: 0.1,
-          ease: "power1.inOut"
+          ease: "power3.inOut"
         },
         // markers: true
       }
     });
       
     // Sección 1: Hero
-    tl.fromTo("#hero-inner", { scale: 0.8, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#hero-inner", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#hero-inner", { scale: 0.8, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 });
+      tl.to("#hero-inner", { scale: 1.5, autoAlpha: 0, duration: 1 });
 
     // Sección 2: Profile
-    tl.fromTo("#profile-inner", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#profile-inner", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#profile-inner", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 });
+      tl.to("#profile-inner", { scale: 1.5, autoAlpha: 0, duration: 1 });
 
     // Sección 3: Skills
-    tl.fromTo("#skills-inner", { scale: 1.2, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#skills-inner", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#skills-inner", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 });
+      tl.to("#skills-inner", { scale: 1.5, autoAlpha: 0, duration: 1 });
 
     // Sección 4: Servicios
-    tl.fromTo("#serv-title", { scale: 1.2, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#serv-title", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#serv-title", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 });
+      tl.to("#serv-title", { scale: 1.5, autoAlpha: 0, duration: 1 });
 
     // Sección 5: Proyecto 1
-    tl.fromTo("#proyecto-1", { scale: 1.2, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#proyecto-1", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#proyecto-1", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 0.5 });
+      tl.to("#proyecto-1", { scale: 1.5, autoAlpha: 0, duration: 3 });
 
     // Sección 6: Proyecto 2
-    tl.fromTo("#proyecto-2", { scale: 1.2, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#proyecto-2", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#proyecto-2", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 0.5 })
+      tl.to("#proyecto-2", { scale: 1.5, autoAlpha: 0, duration: 1 })
 
     // Sección 7: Proyecto 3
-    tl.fromTo("#proyecto-3", { scale: 1.2, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#proyecto-3", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#proyecto-3", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 0.5 })
+      tl.to("#proyecto-3", { scale: 1.5, autoAlpha: 0, duration: 1 })
 
     // Sección 8: Proyecto 4
-    tl.fromTo("#proyecto-4", { scale: 1.2, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#proyecto-4", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#proyecto-4", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 0.5 })
+      tl.to("#proyecto-4", { scale: 1.5, autoAlpha: 0, duration: 1 })
 
     // Sección 9: Preguntas
-    tl.fromTo("#preguntas-contenedor", { scale: 1.2, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
-      .to("#preguntas-contenedor", { scale: 2, autoAlpha: 0, duration: 1 });
+    tl.fromTo("#preguntas-contenedor", { scale: 0.4, autoAlpha: 0 }, { scale: 1, autoAlpha: 1, duration: 1 })
+      tl.to("#preguntas-contenedor", { scale: 1.5, autoAlpha: 0, duration: 1 })
   }, []);
 }
 
